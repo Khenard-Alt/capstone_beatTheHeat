221f6be1592bfce8d0470156d7de58dac1a68e8c
akotosibuboy <klalbaytar1913ant@student.fatima.edu.ph>
2026-09-17 08:11:40 +0800

    mobile developing design for capstone make it like ne

--- frontend/src/pages/principalDash/PrincipalAdvisories.tsx
+++ frontend/src/pages/principalDash/PrincipalAdvisories.tsx
@@ -111,7 +111,7 @@ const PrincipalAdvisories: React.FC = () => {
   };
 
   return (
-    <div id="heat-data-top">
+    <div id="heat-data-top" className="principal-advisories-page">
       <div className="admin-dashboard-header" style={{ marginBottom: 16 }}>
         <div>
           <h1>Advisories</h1>
